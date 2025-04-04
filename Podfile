238e300adb99cedb79b61917c7b8200e49de472b
ainkaspi <ainkaspi@gmail.com>
2025-04-04 23:47:36 +0800

    Initial commit with Sensum project

---
+++ Podfile
@@ -0,0 +1,8 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Sensum' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'MediaPipeTasksVision', '0.10.21'
+end
---
+++ Sensum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sensum
+//
+//  Created by Ain on 04/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
